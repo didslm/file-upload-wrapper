152887660d204ac2d95c609ee0e8cc895b3f91fb
Did <diarselimi92@gmail.com>
2023-03-30 21:15:30 +0200

    check content for file info

--- src/Validation/FileSize.php
+++ src/Validation/FileSize.php
@@ -15,7 +15,7 @@ class FileSize implements ValidatorInterface
     {
         $limitSize = File::ALL_SIZES[$this->type] * $this->size;
 
-        if ($file->getSize() > $limitSize) {
+        if ($this->getUploadedFileSizeMB($file) > $limitSize) {
             throw new ValidationException(
                 sprintf('File size is too big. Limit is %s %s.', $this->size, 'MB'),
                 new ValidationException('Potentialy check you PHP ini configuration, if your file size limit is bigger than the limit you defined in your code.')
@@ -23,6 +23,16 @@ class FileSize implements ValidatorInterface
         }
 
         return true;
+    }
+
+    private function getUploadedFileSizeMB(UploadedFileInterface $uploadedFile): int
+    {
+        $stream = $uploadedFile->getStream();
+        $stream->rewind();
+        $size = $stream->getSize();
+        $stream->close();
+
+        return $size / 1000000;
     }
 
     public function getName(): string
